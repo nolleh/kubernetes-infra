346bedffea446c9d349a3b84c56f92705ed5a1e0
nolleh <nolleh7707@gmail.com>
2019-04-14 21:22:22 +0900

    secret for kiali

--- istio/install.sh
+++ istio/install.sh
@@ -9,6 +9,23 @@ helm init --service-account tiller --upgrade
 
 helm install install/kubernetes/helm/istio-init --name istio-init --namespace istio-system
 
+# KIALI_USERNAME=$(read -p 'Kiali Username: ' uval && echo -n $uval | base64)
+# IALI_PASSPHRASE=$(read -sp 'Kiali Passphrase: ' pval && echo -n $pval | base64)
+
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  name: kiali
+  namespace: $NAMESPACE
+  labels:
+    app: kiali
+type: Opaque
+data:
+  username: $KIALI_USERNAME
+  passphrase: $KIALI_PASSPHRASE
+EOF
+
     #--set "kiali.dashboard.jaegerURL=http://localhost:15030" \
     #--set "kiali.dashboard.grafanaURL=http://localhost:15031" \
 helm install install/kubernetes/helm/istio --name istio --namespace istio-system \
